97b5a56a2876286bb50627d596ffc1f5db95341d
eliaselmokadem <s141765@ap.be>
2024-12-01 00:14:35 +0100

    vercel

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // Import Tailwind CSS
 import App from "./App";
+import "./external/react-refresh/runtime.js";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
