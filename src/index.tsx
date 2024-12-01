0e2cb4362781c669782c557ebd63bd40481e67e3
eliaselmokadem <125689719+eliaselmokadem@users.noreply.github.com>
2024-12-01 17:27:45 +0100

    deleted react-refresh pt2

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // Import Tailwind CSS
 import App from "./App";
-import "./external/react-refresh/runtime.js";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
