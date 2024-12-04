25880cc919cc098858ea724031c947db53f3938c
eliaselmokadem <s141765@ap.be>
2024-12-04 22:59:31 +0100

    logobanner

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
           '100%': { transform: 'translateX(0)', opacity: '1' }
         },
         'slide-up': {
-          '0%': { transform: 'translateY(100%)', opacity: '0' },
+          '0%': { transform: 'translateY(50%)', opacity: '0' },
           '100%': { transform: 'translateY(0)', opacity: '1' }
         }
       },
