40b52e6bc19f64459865610780cc446f460796e3
eliaselmokadem <s141765@ap.be>
2024-12-29 18:10:19 +0100

    coming soon

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
